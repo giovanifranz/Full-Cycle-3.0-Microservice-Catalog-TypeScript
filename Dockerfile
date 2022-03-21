31b9b5324049072784b6121c3f2a144ce971db0b
giovanifranz <giovanifranz@gmail.com>
2022-03-21 02:39:24 -0300

    feat: :arrow_up: atualização do node para utilizar crypto em vez de pacote de terceiro UUID

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14.15.4-slim
+FROM node:14.19.1-slim
 
 USER node 
 
--- src/@seedwork/domain/unique-entity-id.vo.ts
+++ src/@seedwork/domain/unique-entity-id.vo.ts
@@ -1,17 +1,28 @@
-import { v4 as uuidV4, validate as uuidValidate } from "uuid";
+import { randomUUID } from "crypto";
 import InvalidUUIDError from "../errors/invalid-uuid.error";
 
 export default class UniqueEntityId {
   constructor(public readonly id?: string) {
-    this.id = id || uuidV4();
+    this.id = id || randomUUID();
     this.validate();
   }
 
+  private uuidValidate(uuid: string) {
+    const regexTest = uuid.match(
+      "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
+    );
+
+    if (regexTest === null) {
+      return false;
+    }
+    return true;
+  }
+
   private validate() {
-      const isValid = uuidValidate(this.id);
-      if (!isValid) {
-        throw new InvalidUUIDError();
-      }
-      return isValid;
+    const isValid = this.uuidValidate(this.id);
+    if (!isValid) {
+      throw new InvalidUUIDError();
+    }
+    return isValid;
   }
 }
